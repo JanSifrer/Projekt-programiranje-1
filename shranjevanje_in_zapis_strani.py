cf0fc6f5db0ce3ea35b9c8a40a44cff3fa6d5263
Jan Šifrer <jan.sifrer1@gmail.com>
2019-10-31 16:59:09 +0100

    Prenesel podatke iz intrneta - moram še izpopolniti.

--- shranjevanje_in_zapis_strani.py
+++ shranjevanje_in_zapis_strani.py
@@ -3,7 +3,7 @@ import re
 import requests
 import orodja
 
-def shrani_toliko_del(stevilo_prostih_del=50):
+def shrani_toliko_del(stevilo_prostih_del=3017):
     url = 'https://www.studentski-servis.com/index.php?t=prostaDela&hp=true&page=1&perPage={}&sort=1&workType=1&keyword='.format(stevilo_prostih_del)
     orodja.shrani_spletno_stran(url, 'prosta_dela.html')
 
@@ -13,7 +13,7 @@ def vsebina_datoteke(ime_datoteke):
 
 def posamezni_oglasi(ime_datoteke):
     vsebina_strani = vsebina_datoteke(ime_datoteke)
-    vzorec = r'<div class="col jobTitle clear ">(.*?)<div class="col actionBlock">'
+    vzorec = r'<div class="col jobTitle(.*?)<div class="col actionBlock">'
 
     #stevec = 0
     #for zadetek in re.finditer(vzorec, vsebina_strani, flags=re.DOTALL):
@@ -30,23 +30,29 @@ vzorec_oglasa= re.compile(
     r'(?P<lokacija>.*?)</strong>.*?'
     r'<div class="jobContent col">.*?<p>'
     r'(?P<opis>.*?)'
-    r'</p>.*<ul>.*?</strong>'
+    r'</p>.*?<ul>.*?</strong>'
     r'(?P<stevilo_mest>.*?)'
     r'<span.*?</strong>(?P<trajanje>.*?)'
-    r'<span.*?</strong>(?P<delovnik>.*?)'
-    r'(<span.*?<li class.*?/strong>'
-    r'(?P<narava_dela>.*?)</li>)?',
+    r'<span.*?(<strong>Delovnik:</strong>(?P<delovnik>.*?)<span class)'
+    r'.*?<strong>Šifra:</strong>(?P<sifra>.*?)<.*?'
+    r'(<strong>Narava dela:</strong>(?P<narava_dela>.*?)</li.*?)?'
+    r'</ul>',
     flags=re.DOTALL
 )
 
-def podatki_iz_oglasov(ime_datoteke): #to funkcijo moramše popravit!!
-    seznam_oglasov = posamezni_oglasi(ime_datoteke)
-    stevec = 0
-    sez = []
-    for i in range(len(seznam_oglasov)):
-        stevec += 1
-        joj = re.findall(vzorec_oglasa, seznam_oglasov[i])
-        sez += joj
-    print(stevec)
-    print(len(sez))
+
+
+ime_datoteke = 'prosta_dela.html'
+
+def pridobi_podatke_o_oglasih(ime_datoteke):                    #Nekaj vrednosti mi ponovi!
+    vsebina_strani = vsebina_datoteke(ime_datoteke)
+    count = 0
+    slovar=[]
+    for zadetek in re.finditer(vzorec_oglasa, vsebina_strani):
+        slovar_zadetkov = zadetek.groupdict()
+        count += 1
+        slovar.append(slovar_zadetkov)
+
+    orodja.zapisi_csv(slovar, ['naslov', 'lokacija', 'stevilo_mest', 'delovnik', 'narava_dela', 'sifra', 'trajanje', 'naslov', 'podnaslov', 'opis', 'placa'], 'prosta_dela.csv')
+    print('končano:', count)
 
