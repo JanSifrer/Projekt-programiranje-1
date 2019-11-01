96b2a3906b6c4ea154a6cda5b75a813776b860a3
Jan Šifrer <jan.sifrer1@gmail.com>
2019-11-01 12:44:06 +0100

    Popravil kodo, tako da sedaj deluje.

--- shranjevanje_in_zapis_strani.py
+++ shranjevanje_in_zapis_strani.py
@@ -14,23 +14,17 @@ def vsebina_datoteke(ime_datoteke):
 def posamezni_oglasi(ime_datoteke):
     vsebina_strani = vsebina_datoteke(ime_datoteke)
     vzorec = r'<div class="col jobTitle(.*?)<div class="col actionBlock">'
-
-    #stevec = 0
-    #for zadetek in re.finditer(vzorec, vsebina_strani, flags=re.DOTALL):
-       # print(zadetek.groupdict())
-       # stevec += 1
-    #print(stevec)
     return re.findall(vzorec, vsebina_strani, flags=re.DOTALL)
 
 vzorec_oglasa= re.compile(
     r'class="title">(?P<naslov>.*?)'
-    r'(<strong>(?P<podnaslov>.*?)</strong>)?'
-    r'</sp.*?<strong>(?P<placa>.*?)'
-    r'</strong>.*?<strong>'
-    r'(?P<lokacija>.*?)</strong>.*?'
-    r'<div class="jobContent col">.*?<p>'
-    r'(?P<opis>.*?)'
-    r'</p>.*?<ul>.*?</strong>'
+    r'(<strong>(?P<podnaslov>.*?)</strong>)?</sp'
+    r'.*?<strong>(?P<placa>.*?)</strong>'
+    r'.*?<strong>'
+    r'(?P<lokacija>.*?)</strong>'
+    r'.*?<div class="jobContent col">'
+    r'.*?<p>(?P<opis>.*?)(\n.*?)?</p>'
+    r'.*?<ul>.*?</strong>'
     r'(?P<stevilo_mest>.*?)'
     r'<span.*?</strong>(?P<trajanje>.*?)'
     r'<span.*?(<strong>Delovnik:</strong>(?P<delovnik>.*?)<span class)'
@@ -42,17 +36,20 @@ vzorec_oglasa= re.compile(
 
 
 
-ime_datoteke = 'prosta_dela.html'
+#ime_datoteke = 'prosta_dela.html'
 
-def pridobi_podatke_o_oglasih(ime_datoteke):                    #Nekaj vrednosti mi ponovi!
-    vsebina_strani = vsebina_datoteke(ime_datoteke)
-    count = 0
-    slovar=[]
-    for zadetek in re.finditer(vzorec_oglasa, vsebina_strani):
-        slovar_zadetkov = zadetek.groupdict()
-        count += 1
-        slovar.append(slovar_zadetkov)
+def pridobi_podatke_o_oglasih(ime_datoteke):
+    seznam_oglasov = posamezni_oglasi(ime_datoteke)  #seznam po oglasih
+    slovar_iskanih_podatkov = []
+    stevec = 0
+    for i in seznam_oglasov:
+        for zadetek in re.finditer(vzorec_oglasa, i):
+            slovar_zadetkov = zadetek.groupdict()
+            slovar_iskanih_podatkov.append(slovar_zadetkov)
+            stevec += 1
+    print(stevec)
 
-    orodja.zapisi_csv(slovar, ['naslov', 'lokacija', 'stevilo_mest', 'delovnik', 'narava_dela', 'sifra', 'trajanje', 'naslov', 'podnaslov', 'opis', 'placa'], 'prosta_dela.csv')
-    print('končano:', count)
+    orodja.zapisi_csv(slovar_iskanih_podatkov, ['naslov', 'podnaslov', 'placa', 'lokacija', 'opis', 'stevilo_mest', 'trajanje', 'delovnik', 'sifra', 'narava_dela'], 'prosta_dela1.csv')
+    print('končano:', len(slovar_iskanih_podatkov))
+
 
