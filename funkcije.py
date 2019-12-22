6250a8bdecb4ec5292a0999938ba996af1828afb
Jan Šifrer <jan.sifrer1@gmail.com>
2019-12-22 17:03:22 +0100

    Definiral nove funkcije za lepšanje podatkov

--- funkcije.py
+++ funkcije.py
@@ -1,2 +1,103 @@
-x = "CERKLJE NA GORENJSKEM"
+x = "POSTOJNA, SEŽANA, PIVKA, ILIRSKA BISTRICA, CERKLJE NA GORENJSKEM"
 y = x.split()
+
+def boljsa_lokacija(x):
+    razbitje = x.split()
+    if len(razbitje) > 1:
+        if "BLED" in razbitje:
+            return "BLED"
+        if "BLED," in razbitje:
+            return "BLED"
+        if "BOHINJ," in razbitje:
+            return "BOHINJ"
+        if "BOHINJSKA" in razbitje:
+            return "BOHINJSKA-BISTRICA"
+        if "BOROVNICA" in razbitje:
+            return "BOROVNICA"
+        if "BOROVNICA," in razbitje:
+            return "BOROVNICA"
+        if "CELJE" in razbitje:
+            return "CELJE"
+        if "CELJE," in razbitje:
+            return "CELJE"
+        if "DOMŽALE" in razbitje:
+            return "DOMŽALE"
+        if "DOMŽALE," in razbitje:
+            return "DOMŽALE"
+        if "GROSUPLJE" in razbitje:
+            return "GROSUPLJE"
+        if "KRANJ" in razbitje:
+            return "KRANJ"
+        if "KRŠKO" in razbitje:
+            return "KRŠKO"
+        if "KAMNIK" in razbitje:
+            return "KAMNIK"
+        if "KOPER" in razbitje:
+            return "KOPER"
+        if "KONJICE" in razbitje:
+            return "SLOVENKSE-KONJICE"
+        if "KRANJSKA" in razbitje:
+            return "KRANJSKA-GORA"
+        if "LJUBLJANA" in razbitje:
+            return "LJUBLJANA"
+        if "LJ" in razbitje:
+            return "LJUBLJANA"
+        if "LJUBLJANA," in razbitje:
+            return "LJUBLJANA"
+        if "LJUBLJANI" in razbitje:
+            return "LJUBLJANA"
+        if "LJUBLJANE" in razbitje:
+            return "LJUBLJANA"
+        if "LITIJA" in razbitje:
+            return "LITIJA"
+        if "LITIJI" in razbitje:
+            return "LITIJA"
+        if "MOZIRJE" in razbitje:
+            return "MOZIRJE"
+        if "MARIBOR" in razbitje:
+            return "MARIBOR"
+        if "MARIBORU" in razbitje:
+            return "MARIBOR"
+        if "MURSKA" in razbitje:
+            if "SOBOTA" in razbitje:
+                return "MURSKA-SOBOTA"
+        if "MIKLAVŽ" in razbitje:
+            return "MARIBOR"
+        if "NOVO" in razbitje:
+            return "NOVO-MESTO"
+        if "NOVA" in razbitje:
+            if "GORICA" in razbitje:
+                return "NOVA-GORICA"
+        if "PTUJ" in razbitje:
+            return "PTUJ"
+        if "POSTOJNA," in razbitje:
+            return "POSTOJNA"
+        if "PIRAN" in razbitje:
+            return "PIRAN"
+        if "PORTOROŽ" in razbitje:
+            return "PORTOROŽ"
+        if "RADOVLJICA" in razbitje:
+            return "RADOVLJICA"
+        if "RADGONA" in razbitje:
+            return "GORNJA-RADGONA"
+        if "ROGAŠKA" in razbitje:
+            if "SLATINA" in razbitje:
+                return "ROGAŠKA-SLATINA"
+        if "SAP" in razbitje:
+            return "ŠMARJE-SAP"
+        if "SLOVENJ" in razbitje:
+            return "SLOVENJ-GRADEC"
+        if "STARI" in razbitje:
+            return "STARI-VRH"
+        if "ŠTEPANJSKO" in razbitje:
+            return "LJUBLJANA"
+        if "ŠKOFJA" in razbitje:
+            return "ŠKOFJA-LOKA"
+        if "ŽIRI" in razbitje:
+            return "ŽIRI"
+                
+    return x
+
+
+def koliko_besed_je_v_imenu(x):
+    return len(x.split())
